9747b443d1b0205110cacc96bbb089efb00a9f73
nomadHash <badsoap141@gmail.com>
2021-11-28 01:46:39 +0900

    add file-loader & url-loader

--- webpack.client.js
+++ webpack.client.js
@@ -46,6 +46,34 @@ const getConfig = (target) => ({
         test: /\.(scss|css)$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
+      {
+        // write image files under 10k to inline or copy image files over 10k
+        test: /\.(jpg|jpeg|gif|png|svg|ico)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              fallback: 'file-loader',
+              name: 'assets/images/[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        // write files under 10k to inline or copy files over 10k
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              fallback: 'file-loader',
+              name: 'assets/fonts/[name].[ext]',
+            },
+          },
+        ],
+      },
     ],
   },
 
